054bcf71791a4a710239b2f3c7f0767c28ea1384
Mark Scharmann <mark.scharmann@fintiba.com>
2023-12-29 15:25:12 +0100

    figuring out how to server the react app

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	app := fiber.New()
+
+	// serves Single Page application on "/"
+	// assume static file at UI/dist folder
+	app.Static("/", "./UI/dist")
+
+	log.Fatal(app.Listen(":3000"))
 }
