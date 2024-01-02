9fc404893a03c9df63eec88e1bb21f6b5b105b84
m98g <mark.scharmann.ms@gmail.com>
2024-01-02 14:12:07 +0100

    changes

--- UI/src/components/Contact.tsx
+++ UI/src/components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact:React.FC = () => {
         <section className="w-full py-24 bg-gray-100 dark:bg-gray-800" id="contact">
             <div className="container px-4 md:px-6">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-12">Contact</h2>
-                <p className="text-sm text-gray-500 dark:text-gray-400">Email: john.doe@example.com</p>
+                <p className="text-sm text-gray-500 dark:text-gray-400">Email: mark.scharmann.ms@gmail.com</p>
                 <p className="text-sm text-gray-500 dark:text-gray-400">Phone: (123) 456-7890</p>
                 <a className="text-blue-600 hover:text-green-500" href="#">
                     LinkedIn
@@ -13,6 +13,13 @@ const Contact:React.FC = () => {
                 <a className="text-blue-600 hover:text-green-500 ml-4" href="#">
                     GitHub
                 </a>
+                <a className="text-blue-600 hover:text-green-500 ml-4" href="https://www.hackerrank.com/profile/m98_hr">
+                    HackerRank
+                </a>
+            </div>
+
+            <div>
+
             </div>
         </section>
     )
--- UI/src/components/Footer.tsx
+++ UI/src/components/Footer.tsx
@@ -3,13 +3,10 @@ import React from "react";
 const Footer: React.FC = () => {
     return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© John Doe. All rights reserved.</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">© Mark Scharmann. All rights reserved.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
             <a className="text-xs hover:text-green-500" href="#">
-                Terms of Service
-            </a>
-            <a className="text-xs hover:text-green-500" href="#">
-                Privacy Policy
+                Designed using v0 by Vercel, developed and deployed by Me.
             </a>
         </nav>
     </footer>)
