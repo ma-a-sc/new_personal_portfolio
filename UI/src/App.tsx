043e0a03d22f6675dc4a6c35792a2768efce6817
m98g <mark.scharmann.ms@gmail.com>
2024-01-02 01:35:05 +0100

    make it mine muhahahahahah

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
-import Component from "./components/v0_vercel.tsx";
+import Header from "@/components/Header.tsx";
+import Greeting from "@/components/Greeting.tsx";
+import About from "@/components/About.tsx";
+import Skills from "@/components/Skills.tsx";
+import Projects from "@/components/Projects.tsx";
+import Experience from "@/components/Experience.tsx";
+import Education from "@/components/Education.tsx";
+import Contact from "@/components/Contact.tsx";
+import Footer from "@/components/Footer.tsx";
 
 function App() {
-  return (
-    <>
-        <Component />
-    </>
-  )
+    return (
+        <div key="1" className="flex flex-col min-h-[100vh]">
+            <Header/>
+            <main className="flex-1">
+                <Greeting/>
+                <About/>
+                <Skills/>
+                <Projects/>
+                <Experience/>
+                <Education/>
+                <Contact/>
+                <Footer/>
+            </main>
+        </div>
+    )
 }
 
 export default App
