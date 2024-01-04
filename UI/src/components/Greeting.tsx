053cefc3908dcea9a88839b390a9e52de3c5a3e9
Mark Scharmann <mark.scharmann@fintiba.com>
2024-01-04 11:37:35 +0100

    Update Greeting component visibility condition
    Adjusted the visibility condition of the 'Contact Me' button in the Greeting component. Previously, the button was hidden when the expanded state was true. The code has been updated to show the 'Contact Me' button regardless of whether the 'isExpanded' state is true or not.

--- UI/src/components/Greeting.tsx
+++ UI/src/components/Greeting.tsx
@@ -24,20 +24,26 @@ const Greeting: React.FC = () => {
                     <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed dark:text-gray-400">
                         Backend developer specializing in Python and Go. But that is not the only thing I do. If you are curious keep scrolling ... OR
                     </p>
-                    {isExpanded ? null: <a
+                    {isExpanded ? <a
+                        className="opacity-10 hover:cursor-pointer inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+                        {...getToggleProps({onClick: handleClick})}
+                    >
+                        Contact Me
+                    </a> : <a
                         className="hover:cursor-pointer inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gradient-to-r hover:animate-pulse hover:from-green-400 hover:to-yellow-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                    {...getToggleProps({onClick: handleClick})}
-                >
-                    Contact Me
-                </a>
-                }
+                        {...getToggleProps({onClick: handleClick})}
+                    >
+                        Contact Me
+                    </a>
+                    }
 
-                <Card className="w-full max-w-md mx-4 border-t-2 border-b-5 " {...getCollapseProps()}>
-                    <div className={"container flex-row"}>
-                        <button onClick={() => handleClick()} className={"mx-1 my-1 justify-end"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+                    <Card className="w-full max-w-md mx-4 border-t-2 border-b-5 " {...getCollapseProps()}>
+                        <div className={"container flex-row"}>
+                            <button onClick={() => handleClick()} className={"mx-1 my-1 justify-end"}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5"
+                                     stroke="currentColor" className="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
                                 </svg>
                             </button>
                             <CardHeader>
