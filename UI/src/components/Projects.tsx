4fa21a3b03ae49de447e7a4e0b9a268bf8760ab2
Mark Scharmann <mark.scharmann@fintiba.com>
2024-01-15 08:57:39 +0100

    Add toggle feature and enhance the Projects display
    This commit introduces a toggle feature to display all projects and improves project listing in the portfolio. It sets a state to show a predefined number of projects initially and allows loading all projects when required. It also refines the project sorting functionality and enhances the loading display for projects.

--- UI/src/components/Projects.tsx
+++ UI/src/components/Projects.tsx
@@ -4,45 +4,75 @@ import {Project} from "@/interfaces/project.ts";
 import ProjectCard from "@/components/ProjectCard.tsx";
 
 const Projects:React.FC = () => {
+    const [allShown, setAllShown] = React.useState(false)
+    const [itemsToShow, setItemsToShow] = React.useState(3)
     const [projects, setProjects] = React.useState<Project[]>([]);
+
+    const toggle = () => {
+        setAllShown(!allShown)
+        setItemsToShow(projects.length)
+    }
+
     useEffect(() => {
         getProjects().then((response) => {
-        if (response.ok) {
-          response.json().then((data) => {
-            setProjects(data);
-          });
-        }
-      });
+            if (response.ok) {
+                response.json().then((data) => {
+                    setProjects(data);
+                });
+            }
+        });
     }, []);
     useEffect(() => {
-        projects.sort((a, b) => {
-            if (a.Rank < b.Rank) {return -1; }
-            if (a.Rank > b.Rank) {return 1;}
-            return 0;
-        });
+            projects.sort((a, b) => {
+                if (a.Rank < b.Rank) {
+                    return -1;
+                }
+                if (a.Rank > b.Rank) {
+                    return 1;
+                }
+                return 0;
+            });
         }, [projects]
     )
 
     return (
-        <section className="w-full py-24" id="projects">
-            <div className="container px-4 md:px-6">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-12">Projects</h2>
-                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <>
+            <section className="w-full py-24" id="projects">
+                <div className="container px-4 md:px-6">
+                    <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mb-12">Projects</h2>
+                    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+
+                        {
+                            projects.length === 0 ? (
+                                <div>Loading...</div>
+                            ) : (
+                                projects.slice(0, itemsToShow).map((project) => (
+                                    <ProjectCard
+                                        key={project.Rank}
+                                        Title={project.Title}
+                                        Description={project.Description}
+                                        Link={project.Link}
+                                        LinkLabel={project.LinkLabel}
+                                        Internal={project.Internal}
+                                    />
+                                ))
+                            )
+                        }
+
+                    </div>
                     {
-                        projects.map((project) => (
-                            <ProjectCard
-                                key={project.Rank}
-                                Title={project.Title}
-                                Description={project.Description}
-                                Link={project.Link}
-                                LinkLabel={project.LinkLabel}
-                                Internal={project.Internal}
-                            />
-                        ))
+                        allShown ? <> </> : <div className="grid grid-cols-subgrid gap-6 sm:col-span-2 lg:col-span-3">
+                            <a className="text-blue-600 hover:text-green-500 text-center mt-12"
+                               onClick={() => toggle()}>Load
+                                All Projects
+                            </a>
+                        </div>
                     }
                 </div>
-            </div>
-        </section>
+
+            </section>
+
+        </>
     )
 }
 
